addf0ac4ce0f36c0347ef5170bbc9ec1dc1fb04e
alexyanchag <33409665+alexyanchag@users.noreply.github.com>
2025-07-22 02:01:02 -0500

    Enable dark mode and store roles

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -7,6 +7,22 @@
         @if(session('user'))
             <p>Nombre: {{ session('user.nombres') }} {{ session('user.apellidos') }}</p>
             <p>Email: {{ session('user.email') }}</p>
+
+            @if(session('roles'))
+                <h4 class="mt-4">Roles</h4>
+                @foreach(session('roles') as $rol)
+                    <div class="mb-3">
+                        <h5>{{ $rol['nombrerol'] ?? '' }}</h5>
+                        @if(!empty($rol['menu']))
+                            <ul class="ms-3">
+                                @foreach($rol['menu'] as $menu)
+                                    <li>{{ $menu['opcion'] ?? '' }}</li>
+                                @endforeach
+                            </ul>
+                        @endif
+                    </div>
+                @endforeach
+            @endif
         @else
             <p>No has iniciado sesión.</p>
         @endif
