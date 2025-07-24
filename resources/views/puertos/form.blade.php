00f98f5e7e1902429ed6caf70653abb17228d514
alexyanchag <33409665+alexyanchag@users.noreply.github.com>
2025-07-24 16:24:22 -0500

    Add Puerto CRUD

---
+++ resources/views/puertos/form.blade.php
@@ -0,0 +1,20 @@
+@extends('layouts.dashboard')
+
+@section('content')
+<h3>{{ isset($puerto) ? 'Editar' : 'Nuevo' }} Puerto</h3>
+<form method="POST" action="{{ isset($puerto) ? route('puertos.update', $puerto['id']) : route('puertos.store') }}">
+    @csrf
+    @if(isset($puerto))
+        @method('PUT')
+    @endif
+    <div class="mb-3">
+        <label class="form-label">Nombre</label>
+        <input type="text" name="nombre" class="form-control" value="{{ old('nombre', $puerto['nombre'] ?? '') }}" required>
+    </div>
+    @if($errors->any())
+        <div class="alert alert-danger">{{ $errors->first() }}</div>
+    @endif
+    <button type="submit" class="btn btn-primary">Guardar</button>
+    <a href="{{ route('puertos.index') }}" class="btn btn-secondary">Cancelar</a>
+</form>
+@endsection
