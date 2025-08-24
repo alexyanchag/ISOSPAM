4f05e9a874e276856501046a947a99818aec5c7c
alexyanchag <33409665+alexyanchag@users.noreply.github.com>
2025-08-23 20:38:45 -0500

    Use SweetAlert for form messages

--- resources/views/viajes/form.blade.php
+++ resources/views/viajes/form.blade.php
@@ -135,10 +135,18 @@
                     </div>
                 </div>
                 @if(session('success'))
-                    <div class="alert alert-success">{{ session('success') }}</div>
+                    <script>
+                        Swal.fire({icon: 'success', title: 'Éxito', text: @json(session('success'))});
+                    </script>
                 @endif
-                @if($errors->any())
-                    <div class="alert alert-danger">{{ $errors->first() }}</div>
+                @if(session('error'))
+                    <script>
+                        Swal.fire({icon: 'error', title: 'Error', text: @json(session('error'))});
+                    </script>
+                @elseif($errors->any())
+                    <script>
+                        Swal.fire({icon: 'error', title: 'Error', html: `<ul>@foreach($errors->all() as $error)<li>{{ $error }}</li>@endforeach</ul>`});
+                    </script>
                 @endif
             </div>
         </div>
