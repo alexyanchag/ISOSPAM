59efad7b343572324a7a80c288fd34c169c29ff7
alexyanchag <alexyanchag@gmail.com>
2025-08-28 06:39:33 -0500

    mejoras

--- resources/views/viajes/pendientes.blade.php
+++ resources/views/viajes/pendientes.blade.php
@@ -4,9 +4,6 @@
 <div class="card">
     <div class="card-header d-flex justify-content-between align-items-center">
         <h3 class="card-title mb-0">Viajes pendientes</h3>
-        <div class="card-tools">
-            <a href="{{ route('viajes.index') }}" class="btn btn-primary btn-sm">Volver</a>
-        </div>
     </div>
     <div class="card-body">
         <div class="table-responsive">
