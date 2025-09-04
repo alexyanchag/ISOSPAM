d688f18ee7f1bfc25fd46a1733a66627127ddf71
alexyanchag <33409665+alexyanchag@users.noreply.github.com>
2025-09-04 00:04:24 -0500

    feat: render menu from session tree

---
+++ app/View/Components/MenuTree.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace App\View\Components;
+
+use Illuminate\View\Component;
+use Illuminate\Support\Collection;
+
+class MenuTree extends Component
+{
+    /**
+     * All menus available for rendering.
+     *
+     * @var \Illuminate\Support\Collection
+     */
+    public Collection $menus;
+
+    /**
+     * Parent menu id.
+     *
+     * @var int|null
+     */
+    public $parentId;
+
+    /**
+     * Create a new component instance.
+     *
+     * @param  iterable  $menus
+     * @param  int|null  $parentId
+     * @return void
+     */
+    public function __construct(iterable $menus, $parentId = null)
+    {
+        $this->menus = collect($menus)->map(fn ($item) => (object) $item);
+        $this->parentId = $parentId;
+    }
+
+    /**
+     * Get the view / contents that represent the component.
+     */
+    public function render()
+    {
+        return view('components.menu-tree');
+    }
+}
